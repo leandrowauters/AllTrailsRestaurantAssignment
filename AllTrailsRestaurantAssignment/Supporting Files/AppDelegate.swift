336204889246f397275672c7e39ec09376af237b
Leandro Wauters <leandro.wauters@gmail.com>
2021-08-27 19:11:16 -0400

    Initial Setup completed

---
+++ AllTrailsRestaurantAssignment/Supporting Files/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  AllTrailsRestaurantAssignment
+//
+//  Created by Leandro Wauters on 8/27/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window:UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+       
+        // Override point for customization after application launch.fgf
+        return true
+    }
+
+
+}
