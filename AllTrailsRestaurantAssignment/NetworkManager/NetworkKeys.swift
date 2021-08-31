b1cfd14b35593e8f322add36743a0ad89b5c996e
leandrowauters <leandro.wauters@gmail.com>
2021-08-31 14:35:56 -0400

    Refactored

---
+++ AllTrailsRestaurantAssignment/NetworkManager/NetworkKeys.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkKeys.swift
+//  AllTrailsRestaurantAssignment
+//
+//  Created by Leandro Wauters on 8/27/21.
+//
+
+import Foundation
+
+struct NetworkKeys {
+    // ADD API KEY
+    static let googleAPIKey = "API KEY HERE"
+}
